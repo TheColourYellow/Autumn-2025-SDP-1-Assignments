e833529a51d98ca57a87f75dfc1379810ee85e6e
Patrik <Patrik.Granstrom@metropolia.fi>
2025-09-08 16:01:22 +0300

    Add completed assignment

---
+++ src/test/java/TemperatureConverterTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureConverterTest {
+    private final TemperatureConverter tempConv = new TemperatureConverter();
+
+    @Test
+    void testFahrenheitToCelsius() {
+        assertEquals(-2.77, tempConv.fahrenheitToCelsius(27));
+    }
+
+    @Test
+    void testCelsiusToFahrenheit() {
+        assertEquals(212, tempConv.celsiusToFahrenheit(100));
+        assertEquals(179.59, tempConv.celsiusToFahrenheit(82));
+    }
+
+    @Test
+    void testIsExtremeTemperature() {
+        assertTrue(tempConv.isExtremeTemperature(-50));
+        assertTrue(tempConv.isExtremeTemperature(90));
+        assertFalse(tempConv.isExtremeTemperature(27));
+    }
+}
